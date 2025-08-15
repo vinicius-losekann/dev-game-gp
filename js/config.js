5a67ea64ed700245480e599cc7bedfed48d25a1b
vinicius-losekann <vinicius.losekann@ufsm.br>
2025-08-15 14:04:44 -0300

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,8 @@
+const AppConfig = {
+    // Define o idioma padrão da aplicação.
+    // Pode ser 'pt-BR', 'en-US', 'es-ES' ou qualquer outro idioma que você suporte.
+    defaultLanguage: 'pt-BR', 
+
+    // Adicione outras configurações globais aqui, se necessário.
+    // Exemplo: apiUrl: 'https://api.seujogo.com/v1',
+};
